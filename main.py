d291057cfeab287381d6d2ff2cf18c4760df3cf2
ava rongo <arongo@binghamton.edu>
2024-05-05 02:57:30 -0400

    done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.controller import Controller
   
 if __name__ == "__main__":
-    game_controller = Controller()
-    game_controller.start() 
+    controller = Controller()
+    controller.start() 
